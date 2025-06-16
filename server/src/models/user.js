930976c0b824a05b0431a2941eb45964413c1a20
Recep Demir <demir.rp@gmail.com>
2025-06-16 22:03:24 +0200

    Add user schema definition with validation and default fields

--- server/src/models/user.js
+++ server/src/models/user.js
@@ -1,2 +1,60 @@
 "use strict"
 const { mongoose } = require('../configs/dbConnection')
+
+const userSchema = new mongoose.Schema({
+
+    username: {
+        type: String,
+        trim: true,
+        required: true,
+        unique: true,
+        index: true
+    },
+
+    password: {
+        type: String,
+        trim: true,
+        required: true,
+        // set: ()=> 'encyrpte password'
+    },
+
+    email: {
+        type: String,
+        trim: true,
+        required: true,
+        unique: true,
+        index: true
+    },
+
+    firstName: {
+        type: String,
+        trim: true,
+        required: true,
+    },
+
+    lastName: {
+        type: String,
+        trim: true,
+        required: true,
+    },
+
+    isActive: {
+        type: Boolean,
+        default: true
+    },
+
+    isStaff: {
+        type: Boolean,
+        default: false
+    },
+
+    isAdmin: {
+        type: Boolean,
+        default: false
+    },
+
+}, {
+    collection: 'users',
+    timestamps: true
+});
+
