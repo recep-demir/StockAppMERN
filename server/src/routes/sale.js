c016d1601e34e6e975764e10e2d4abda8ac3c9fc
Recep Demir <demir.rp@gmail.com>
2025-06-18 21:28:09 +0200

    Add sale routes with CRUD operations

--- server/src/routes/sale.js
+++ server/src/routes/sale.js
@@ -1,3 +1,12 @@
 "use strict"
 
 const router = require('express').Router()
+
+
+const { list, create, read, update, deletee } = require('../controllers/sale');
+
+router.route('/').get(list).post(create);
+
+router.route('/:id').get(read).put(update).patch(update).delete(deletee);
+
+module.exports = router;
