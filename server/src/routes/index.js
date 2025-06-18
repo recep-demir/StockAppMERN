ecc46e9e1866622a4b1298d1dbf6e2736dfc2a98
Recep Demir <demir.rp@gmail.com>
2025-06-18 21:27:57 +0200

    Refactor route definitions for better organization and readability

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -5,4 +5,35 @@ const router = require('express').Router()
 
 router.use('/documents', require('./document'))
 
+
+// auth:
+// router.use('/auth', require('./auth'));
+
+// user:
+router.use('/users', require('./user'));
+
+// token:
+router.use('/tokens', require('./token'));
+
+// brand:
+router.use('/brands', require('./brand'));
+
+// category:
+router.use('/categories', require('./category'));
+
+// firm:
+router.use('/firms', require('./firm'));
+
+// product:
+router.use('/products', require('./product'));
+
+// purchase:
+router.use('/purchases', require('./purchase'));
+
+// sale:
+router.use('/sales', require('./sale'));
+
+// document:
+router.use('/documents', require('./document'));
+
 module.exports = router
