bac074415fb032bca317c0b087e57937d65c1833
Recep Demir <demir.rp@gmail.com>
2025-06-24 20:49:45 +0200

    Refactor auth controller to enhance login functionality and improve error handling

--- server/src/controllers/auth.js
+++ server/src/controllers/auth.js
@@ -1,3 +1,59 @@
 " use strict"
 
-const User = require("../models/user")
+const User = require("../models/user");
+const Token = require('../models/token');
+const passwordEncrypt = require('../helpers/passwordEncrypt');
+const jwt = require('jsonwebtoken');
+const CustomError = require("../helpers/customError");
+
+module.exports = {
+
+    login: async (req, res) => {
+        /*
+            #swagger.tags = ["Authentication"]
+            #swagger.summary = "Login"
+            #swagger.description = 'Login with username (or email) and password for get Token and JWT.'
+            #swagger.parameters["body"] = {
+                in: "body",
+                required: true,
+                schema: {
+                    "username": "test",
+                    "password": "1234",
+                }
+            }
+        */
+
+        if (!((req.body.email || req.body.username) && req.body.password)) throw new CustomError('Please enter username/email and password.');
+
+        const user = await User.findOne({ $or: [{ username: req.body.username }, { email: req.body.email }] });
+
+        if (!user && user.password !== passwordEncrypt(req.body.password)) throw new CustomError('Wrong email/username or password');
+
+        if (!user.isActive) throw new CustomError('This account is not active.');
+
+        /* Simple Token */
+        let tokenData = await Token.findOne({ userId: user._id });
+        if (!tokenData) {
+            tokenData = await Token.create({
+                userId: user._id,
+                token: passwordEncrypt(user._id + Date.now())
+            });
+        };
+
+        /* JWT Token */
+        const { _id, email, password, ...accessPayload } = user._doc;
+
+        const accessToken = jwt.sign(accessPayload, process.env.ACCESS_KEY, { expiresIn: '30m' });
+        const refreshToken = jwt.sign({ _id }, process.env.REFRESH_KEY, { expiresIn: '1d' });
+
+        res.status(200).send({
+            error: false,
+            token: tokenData.token,
+            user,
+            bearer: { accessToken, refreshToken }
+        });
+
+    },
+
+
+}
