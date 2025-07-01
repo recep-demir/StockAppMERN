130b3dcf793333a47a18490959096d4fe91c43e7
Recep Demir <demir.rp@gmail.com>
2025-07-01 20:59:27 +0200

    Refactor auth route file structure for improved readability

--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -1,3 +1,11 @@
 "use strict"
 
 const router = require('express').Router()
+
+const { login, refresh, logout } = require('../controllers/auth');
+
+router.post('/login', login);
+router.post('/refresh', refresh);
+router.post('/logout', logout);
+
+module.exports = router;
