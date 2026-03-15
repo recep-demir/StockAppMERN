348e45f75c6d25c24d41ba042dee733ebdc7024b
Recep Demir <recep.demir@powercoders.org>
2026-03-15 01:13:49 +0100

    fix: update AuthHeader component styles and typography for improved presentation

--- client/src/components/AuthHeader.jsx
+++ client/src/components/AuthHeader.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
+
 const AuthHeader = () => {
   return (
-    <Grid item xs={12} mb={3}>
-      <Typography variant="h3" color="primary" align="center">
+    <Grid item xs={12} sx={{ mt: 10, mb: 1 }}>
+      <Typography 
+        variant="h2" 
+        color="primary" 
+        align="center"
+        sx={{ 
+          fontWeight: 700, 
+          width: "100%" 
+        }}
+      >
         Stock Management App
       </Typography>
     </Grid>
